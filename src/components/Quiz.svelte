2af4e163c0dcf1cb87bb8d049bae3b666a86b6b2
shawncocklin <shawncocklin@gmail.com>
2022-03-11 08:41:13 -0500

    hooked into quiz API

--- src/components/Quiz.svelte
+++ src/components/Quiz.svelte
@@ -1,10 +1,49 @@
 <script>
-  import Button from './Button.svelte'
+  const ENDPOINT =
+    'https://opentdb.com/api.php?amount=10&category=22&type=multiple'
+  let correctAnswer = 'b'
   let answers = ['a', 'b', 'c', 'd']
+
+  let quiz = getQuiz()
+
+  let result = ''
+
+  function pickAnswer(answer) {
+    if (answer == correctAnswer) {
+      return (result = 'Correct')
+    }
+    result = 'whOOPSIE'
+  }
+
+  async function getQuiz() {
+    const res = await fetch(ENDPOINT)
+    const quiz = await res.json()
+    console.log(quiz)
+    return quiz
+  }
+
+  function handleClick() {
+    quiz = getQuiz()
+  }
 </script>
 
 <div>
+  {#if result}
+    <h4>{result}</h4>
+  {:else}
+    <h5>Pick and answer</h5>
+  {/if}
+
+  {#await quiz}
+    loading...
+  {:then data}
+    <h3>{data.results[0].question}</h3>
+  {/await}
+
+  <button on:click={handleClick}>Get Quiz</button>
   {#each answers as answer}
-    <Button selectedAnswer={answer} />
+    <button on:click={() => pickAnswer(answer)}>Answer {answer}</button>
   {/each}
 </div>
+
+<style></style>
