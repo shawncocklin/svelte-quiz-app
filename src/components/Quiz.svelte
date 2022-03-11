4bde4c92ef6193ad2b268fad2d07fcf32c28ea3e
shawncocklin <shawncocklin@gmail.com>
2022-03-10 19:55:58 -0500

    scaffolded button component

---
+++ src/components/Quiz.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Button from './Button.svelte'
+  let answers = ['a', 'b', 'c', 'd']
+</script>
+
+<div>
+  {#each answers as answer}
+    <Button selectedAnswer={answer} />
+  {/each}
+</div>
